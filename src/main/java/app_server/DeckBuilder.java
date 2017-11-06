0b47164d056e3145be1e50a56a71ae8e3210305d
Sienaert <sander.sienaert@hotmail.com>
2017-11-06 16:33:52 +0100

    Deckbuilder with images fixed

--- src/main/java/app_server/DeckBuilder.java
+++ src/main/java/app_server/DeckBuilder.java
@@ -27,7 +27,7 @@ public class DeckBuilder {
             for (Card.CardColor color : Card.CardColor.values()) {
                 //Generate all numbered cards
                 for (Number number : Number.values()) {
-                   /* BufferedImage img = null;
+                    BufferedImage img = null;
                     try {
                         img = ImageIO.read(new File(getClass().getResource("/textures/" + number + "_" + color + ".png").toURI()));
                     } catch (Exception e) {
@@ -35,8 +35,7 @@ public class DeckBuilder {
                         LOGGER.info("Error loading card image: "+"/textures/" + number + "_" + color + ".png");
                     }
                     LOGGER.log(Level.INFO,"Creating normal card");
-                    Card card = new Card(SwingFXUtils.toFXImage(img, null), Card.CardType.NORMAL, color, number.getValue());*/
-                    Card card = new Card(null, Card.CardType.NORMAL, color, number.getValue());
+                    Card card = new Card(SwingFXUtils.toFXImage(img, null), Card.CardType.NORMAL, color, number.getValue());
                     LOGGER.log(Level.INFO,"Created normal card");
                     deck.add(card);
 
@@ -52,33 +51,30 @@ public class DeckBuilder {
 
                     if(i==0 && (cardType == Card.CardType.PLUS4 || cardType == Card.CardType.PICK_COLOR)){
                         //Make each special uncolored card four times, once for each color only in first iteration
-                       /* try {
+                        try {
                             img = ImageIO.read(new File(getClass().getResource("/textures/" + cardType + ".png").toURI()));
                         } catch (Exception e) {
                             LOGGER.info("Error loading card image :" + "/textures/" + cardType + ".png");
-                        }*/
+                        }
                         LOGGER.log(Level.INFO,"Creating PLUS4/COLOR card");
-                        //deck.add(new Card(SwingFXUtils.toFXImage(img, null), cardType, null, null));
-                        deck.add(new Card(null, cardType, null, null));
-
+                        deck.add(new Card(SwingFXUtils.toFXImage(img, null), cardType, null, null));
                     }
 
-                    /*try {
-                        img = ImageIO.read(new File(getClass().getResource("/textures/" + cardType + "_" + color + ".png").toURI()));
-                    } catch (Exception e) {
-                        LOGGER.info("Error loading card image : "+"/textures/" + cardType + "_" + color + ".png");
-                    }*/
-                    LOGGER.log(Level.INFO,"Creating special card: "+cardType.toString()+", "+color.toString());
-                    //deck.add(new Card(SwingFXUtils.toFXImage(img, null), cardType, color, null));
-                    Card card = new Card(null, cardType, color, null);
-                    LOGGER.log(Level.INFO,"Created special card");
-                    deck.add(card);
-
+                    if(cardType == Card.CardType.PLUS2 || cardType == Card.CardType.SKIP || cardType == Card.CardType.REVERSE) {
+                        try {
+                            img = ImageIO.read(new File(getClass().getResource("/textures/" + cardType + "_" + color + ".png").toURI()));
+                        } catch (Exception e) {
+                            LOGGER.info("Error loading card image : " + "/textures/" + cardType + "_" + color + ".png");
+                        }
+                        LOGGER.log(Level.INFO, "Creating special card: " + cardType.toString() + ", " + color.toString());
+                        deck.add(new Card(SwingFXUtils.toFXImage(img, null), cardType, color, null));
+                    }
                 }
             }
         }
 
-        LOGGER.log(Level.INFO, "Deck created");
+        LOGGER.log(Level.INFO, "Deck size: "+deck.size());
+
         Collections.shuffle(deck);
         return deck;
     }
