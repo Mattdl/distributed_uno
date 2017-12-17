7657e7025a08e37453910ddfa7001d1b417b913a
mattdl <de.lange.matthias@gmail.com>
2017-12-17 15:21:15 +0100

    salt fixke

--- src/main/java/app_server/service/RegisterService.java
+++ src/main/java/app_server/service/RegisterService.java
@@ -1,35 +1,36 @@
 package app_server.service;
 
-import db_server.UserDbService;
-import model.Passwords;
 import model.User;
+import security.Passwords;
 import stub_RMI.appserver_dbserver.UserDbStub;
 import stub_RMI.client_appserver.RegisterStub;
-import sun.security.util.Password;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class RegisterService extends UnicastRemoteObject implements RegisterStub {
-    private  UserDbStub userDbService;
-    private byte[] salt;
+    private UserDbStub userDbService;
+
 
     public RegisterService(UserDbStub userDbService) throws RemoteException {
         this.userDbService = userDbService;
-        //Create new salt for current user
-        salt = Passwords.getNextSalt();
     }
 
-    public boolean register(String username, String password){
+    public boolean register(String username, String password) {
+
+        byte[] salt = Passwords.getNextSalt();
+
         //Hash password
         byte[] hash = Passwords.hash(password.toCharArray(), salt);
 
         boolean successful = false;
 
-        User user = new User(username, new String(hash), salt.toString());
+        User user = new User(username, new String(hash), new String(salt));
 
         try {
+
             successful = userDbService.persistUser(user, true);
+
         } catch (RemoteException e) {
             e.printStackTrace();
         }
